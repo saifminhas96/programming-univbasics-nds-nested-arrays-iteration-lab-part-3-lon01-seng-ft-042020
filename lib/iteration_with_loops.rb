be7ac5dcfac37e02be1ff867d815879f9fac1569
Saif Minhas <saif_minhas@hotmail.com>
2020-04-12 10:19:25 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,12 +1,4 @@
 def join_nested_strings(src)
-words = []
-mixed_data_1.each do |items|
-  items.each do |item|
-    if item.is_a?(String)
-      words << item
-    end
-  end
-end
-
-[words.join(" ")]
+  # src will be an Array of Arrays of Strings and Integers
+  # Combine all Strings present in the AoA into a single value and return it
 end
